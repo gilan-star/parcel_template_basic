3e3201b0f5b5388880dd3b2cc7a1ce241930f4e0
gilan-star <83273499+gilan-star@users.noreply.github.com>
2021-08-12 18:48:22 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+// In node.js, using commonJS (instead of ESM, ECMAScriptModule)
+
+// require() (= import in ESM)
+// module.exports (= export in ESM)
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
